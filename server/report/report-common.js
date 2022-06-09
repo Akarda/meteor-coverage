c44a33c4e4ef19c9f5f3df5d0ee252ab008c7f48
Andreas Schormüller <whitekelt@googlemail.com>
2022-06-10 01:25:24 +0200

    format comment

--- server/report/report-common.js
+++ server/report/report-common.js
@@ -4,15 +4,13 @@ import path from 'path';
 import Log from './../context/log';
 const Report = Npm.require('istanbul-lib-report');
 
-
 export default ReportCommon = {
   /**
-     * Alter fs to add a new method writer
-     * Used by the istanbul-reports library
-     * @param filepath
-     * @returns {*|Context}
-     */
-
+   * Alter fs to add a new method writer
+   * Used by the istanbul-reports library
+   * @param filepath
+   * @returns {*|Context}
+   */
   getContext (filepath) {
     let context = Report.createContext();
 
